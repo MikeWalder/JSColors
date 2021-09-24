ef56cd2f28183048f77c73585c3ffc3002e911f0
MikeWalder <mike_walder@hotmail.fr>
2021-09-24 16:28:08 +0200

    Version 2

--- script_JSColors.js
+++ script_JSColors.js
@@ -1,3 +1,4 @@
+const mainTitle = document.querySelector('h1');
 const elt = document.querySelector('#rectangle');
 const inputColor = document.querySelector('#render');
 const colorRender = document.querySelector('#colorRender');
@@ -33,52 +34,54 @@ elt.addEventListener('click', function(e) {
     const divY = elt.offsetHeight; 
 
     const total = (x - eltInfos.x + y - eltInfos.y).toPrecision(4);
-    const totalHue = (360 *total * (712 / 710) / (divX + divY)).toPrecision(4);
+    const totalHue = (360 * total * (712 / 710) / (divX + divY)).toPrecision(4);
     const saturationX = 100 * (x - eltInfos.x) / divX;
 
     const elemColored = document.createElement('div');
     
     elt.appendChild(elemColored);
-
+    
     elemColored.classList.add('colored');
+    const visualColored = document.querySelectorAll('#rectangle .colored');
+    let tabVisualColored = [...visualColored];
+    
     elemColored.style.left = (x - 5)+"px";
     elemColored.style.top = (y - 5)+"px";
-    
     elemColored.style.backgroundColor=`hsl(${totalHue}, ${saturationX}%, 50%)`;
-    elemColored.style.transition = 'background-color 3s ease';
 
+    /* Ajout d'une div dans l'article contenant la couleur sélectionnée
+    dans la palette, le rgb correspondant et l'icône de suppression de la div */
     const divRgbTexted = document.createElement('div');
-    const rgbTexted = document.createElement('label');
     const renderRgbTexted = document.createElement('input');
+    const fontDelete = document.createElement('i');
+    const rgbTexted = document.createElement('label');
     
     article.appendChild(divRgbTexted);
     divRgbTexted.appendChild(renderRgbTexted);
+    divRgbTexted.appendChild(fontDelete);
     divRgbTexted.appendChild(rgbTexted);
-    
+
     divRgbTexted.classList.add(`tabColor${k}`);
-    divRgbTexted.title="Cliquez pour supprimer";
+    fontDelete.classList.add(`deleting${k}`);
+
+    // Ajout de l'icône trash de font awesome
+    fontDelete.classList.add('fas');
+    fontDelete.classList.add('fa-trash-alt');
 
     renderRgbTexted.classList.add('inputedColor');
     renderRgbTexted.style.backgroundColor = `${inputColor.style.backgroundColor}`;
     rgbTexted.innerHTML = `${k+1}) ${inputColor.style.backgroundColor}`;
     k++;
     
+    // traitement de chaque div dans un tableau pour traiter le cas de la suppression
     divColored = document.querySelectorAll('article div');
     let tabColored = [...divColored];
 
+    // Gestion de la suppression de chaque div dans article
     for(let j = 0; j < tabColored.length; j++) {
         tabColored[j].addEventListener('click', function() {
-            this.innerHTML = ""; // suppression fictive de l'élément à supprimer
-            tabColored.splice(this, 1);
+            this.remove();; // suppression de la div 
+            tabVisualColored[j].remove(); // Suppression du cercle de couleur correspondant à la div supprimée
         })
     }
 })
-
-
-/* Améliorations prochaine version : 
-
-1) Ajouter des animations simples
-2) Supprimer du DOM la div sélectionnée dans <article> 
-3) Ajouter Un bouton "supprimer" en favicon remplaçant le title au hover de chaque label
-
-*/
